1c1caa7ef857734bb349c4c981463a515b9f767d
Luis Rondon <leduardo.rondon@gmail.com>
2022-10-10 16:09:04 -0400

    update packages

--- src/helpers/Config.js
+++ src/helpers/Config.js
@@ -1,7 +1,7 @@
 export default config = {
     url: {
         production: false,
-        dev: 'http://localhost:9010/api/v1/', 
+        dev: 'http://localhost:9010/api/v1/',
         prod: 'http://localhost/api/v1/'
     },
     headers: {
